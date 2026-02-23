a19b1c3a1afc3836e8e98b652caccecbf0fd42c0
viktoria naskova <smorodinka-gg@mail.ru>
2026-02-23 17:36:10 +0300

    fix: correct gettoken call as a function, add check for null

--- frontend/src/hooks/useUserSync.js
+++ frontend/src/hooks/useUserSync.js
@@ -1,16 +1,24 @@
 import { useEffect, useRef } from "react";
-import { useUser } from "@clerk/clerk-react";
+import { useAuth, useUser } from "@clerk/clerk-react";
 import { syncUser } from "../api/syncUser";
 
 export const useSyncUser = () => {
-  const { isLoaded, isSignedIn, getToken } = useUser();
+  const { isLoaded, isSignedIn } = useUser();
+  const { getToken } = useAuth();
   const syncedRef = useRef(false);
 
   useEffect(() => {
     if (!isLoaded || !isSignedIn || syncedRef.current) return;
 
     (async () => {
-      const token = await getToken;
+      const token = await getToken();
+      console.log("Токен получен");
+
+      if (!token) {
+        console.log("Токен не получен");
+        return;
+      }
+
       await syncUser(token);
       syncedRef.current = true;
     })();
