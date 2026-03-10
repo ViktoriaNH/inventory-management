3c8f18ea1e63f66a2ce3397d0591f87d410c3c6e
viktoria naskova <smorodinka-gg@mail.ru>
2026-03-10 19:33:27 +0300

    style: add truncate for descriptions

--- frontend/src/helpers/render-cell-value.jsx
+++ frontend/src/helpers/render-cell-value.jsx
@@ -3,8 +3,14 @@ import ReactMarkdown from "react-markdown";
 
 export const renderCellValue = (row, column, value) => {
   return (
-    column.type === "link" ? <Link className='link-dark text-decoration-none' to={column.getLink(row)}>{value}</Link>
-    : column.type === "markdown" ? <ReactMarkdown>{value}</ReactMarkdown>
+    column.type === "link" ?
+      <Link className="link-dark text-decoration-none" to={column.getLink(row)}>
+        {value}
+      </Link>
+    : column.type === "markdown" ?
+      <div className="text-truncate">
+        <ReactMarkdown>{value}</ReactMarkdown>
+      </div>
     : value
   );
 };
