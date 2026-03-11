3bcf7dfbfeefb1d5a6f7d8c9ec516214b0377904
viktoria naskova <smorodinka-gg@mail.ru>
2026-03-12 00:04:28 +0300

    fix(controller): correct get id in params

--- backend/src/controllers/get-inventory-by-id.js
+++ backend/src/controllers/get-inventory-by-id.js
@@ -2,7 +2,7 @@ import { getInventoryById } from "../services/inventory.js";
 import { handleError } from "../utils/handle-error.js";
 
 export const getInventoryByIdController = async (req, res) => {
-  const { id } = req.params();
+  const id  = req.params.inventoryId;
   try {
     const inventories = await getInventoryById(id);
     res.status(200).json(inventories);
