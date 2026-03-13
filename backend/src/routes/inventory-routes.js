4944325ff67e85efd6a4cc1388b0b51ed1f1c3e6
viktoria naskova <smorodinka-gg@mail.ru>
2026-03-13 14:30:54 +0300

    fix: delete unnecessary import

--- backend/src/routes/inventory-routes.js
+++ backend/src/routes/inventory-routes.js
@@ -4,8 +4,6 @@ import { createInventoryController } from "../controllers/create-inventory.js";
 import { getLatestInventoriesController } from "../controllers/get-latest-inventory.js";
 import { getMyInventoryController } from "../controllers/get-my-inventory.js";
 import { getInventoryByIdController} from '../controllers/get-inventory-by-id.js'
-import { deleteInventoryControllel } from "../controllers/delete-inventory.js";
-
 
 const router = Router();
 
@@ -14,6 +12,5 @@ router.get("/latest", getLatestInventoriesController);
 router.get("/my", getMyInventoryController);
 router.post("/create", createInventoryController);
 router.get("/:inventoryId", getInventoryByIdController);
-router.delete("/delete", deleteInventoryControllel);
 
 export default router;
