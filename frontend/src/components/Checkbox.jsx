378c1b4021df7aa8d4d105520843253674f1f600
viktoria naskova <smorodinka-gg@mail.ru>
2026-03-14 12:12:56 +0300

    style(ui): add styles for custom checkbox

--- frontend/src/components/Checkbox.jsx
+++ frontend/src/components/Checkbox.jsx
@@ -1,12 +1,13 @@
 export const Checkbox = ({ name, label, isChecked, onChange }) => {
   return (
-    <div className="form-check">
+    <div className="form-check" style={{ "--bs-primary": "#6c757d" }}>
       <input
         className="form-check-input shadow-none"
         type="checkbox"
         onChange={onChange}
         checked={isChecked}
         id={name}
+  
       />
       {label && (
         <label className="form-check-label" htmlFor={name}>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import './index.css'
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { StrictMode } from "react";
