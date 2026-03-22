83571f0985c41d5b126a804b3a269517a999c73b
viktoria naskova <smorodinka-gg@mail.ru>
2026-03-22 23:51:44 +0300

    feat(modal): add try/catch for correct closing

--- frontend/src/utils/nandle-modal-submit.js
+++ frontend/src/utils/nandle-modal-submit.js
@@ -1,12 +1,16 @@
 export const handleModalSubmit = (modalId, onSubmit) => {
   return async (data, reset) => {
-    await onSubmit(data);
+    try {
+      await onSubmit(data);
 
-    const modalElement = document.getElementById(modalId);
-    const modalInstance =
-      window.bootstrap.Modal.getOrCreateInstance(modalElement);
+      const modalElement = document.getElementById(modalId);
+      const modalInstance =
+        window.bootstrap.Modal.getOrCreateInstance(modalElement);
 
-    reset();
-    modalInstance.hide();
+      reset();
+      modalInstance.hide();
+    } catch (error) {
+      console.error(error);
+    }
   };
 };
