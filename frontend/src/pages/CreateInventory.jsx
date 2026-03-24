8da8adc7e4ced54fcf4d943a5376251d3293d864
viktoria naskova <smorodinka-gg@mail.ru>
2026-03-25 01:37:30 +0300

    fix: delete  remove unnecessary isAuth null check in CreateInventoryPage

--- frontend/src/pages/CreateInventory.jsx
+++ frontend/src/pages/CreateInventory.jsx
@@ -1,13 +1,10 @@
 import { CreateInventoryForm } from "../components/Form/CreateInventoryForm";
 import { ALERT_MESSAGES } from "../data/alert-messages";
 import { useClerkAuthStatus } from "../hooks/useClerkAuthStatus";
-import { Header } from "../layouts/Header";
 import { PageWrapper } from "../layouts/PageWrapper";
 
 export const CreateInventory = () => {
   const { isAuth } = useClerkAuthStatus();
-
-  if (!isAuth) return null;
 
   return (
     <PageWrapper>
