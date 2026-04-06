d819cc5ecc69362dcc9e99cbe024ab8424b73d11
viktoria naskova <smorodinka-gg@mail.ru>
2026-04-06 16:55:20 +0300

    feat: add PageWrapper for PageSearch

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -5,6 +5,7 @@ import { MAINPAGE_INVENTORIES_COLUMNS } from "../data/columns";
 import { fetchSearchByText } from "../api/search-api";
 import { useApi } from "../hooks/useApi";
 import { QUERY_KEYS } from "../data/queries";
+import { PageWrapper } from "../layouts/PageWrapper";
 
 export const SearchPage = () => {
   const [searchParams] = useSearchParams();
@@ -12,11 +13,13 @@ export const SearchPage = () => {
   const searchText = searchParams.get("query");
 
   return (
-    <InventoryTable
-      title={SECTION_LABELS.FOUND_INVENTORIES}
-      queryKey={QUERY_KEYS.text.searchByText(searchText)}
-      fetch={() => fetchSearchByText(api, searchText)}
-      columns={MAINPAGE_INVENTORIES_COLUMNS}
-    />
+    <PageWrapper>
+      <InventoryTable
+        title={SECTION_LABELS.FOUND_INVENTORIES}
+        queryKey={QUERY_KEYS.text.searchByText(searchText)}
+        fetch={() => fetchSearchByText(api, searchText)}
+        columns={MAINPAGE_INVENTORIES_COLUMNS}
+      />
+    </PageWrapper>
   );
 };
