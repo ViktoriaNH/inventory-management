fa5e7e8314b76afebb04c4484f8fdff3382aa7c3
viktoria naskova <smorodinka-gg@mail.ru>
2026-04-06 21:06:30 +0300

    feat(services): update search logic with join

--- backend/src/services/search.js
+++ backend/src/services/search.js
@@ -2,9 +2,15 @@ import { prisma } from "../../prisma/client.js";
 
 export const searchByText = async (text) => {
   const result = await prisma.$queryRaw`
-    SELECT title
-    FROM "inventories"
-    WHERE search_vector @@ plainto_tsquery('english', ${text})
+    SELECT 
+      i.id,
+      i.title,
+      i.description,
+      u.name AS "creatorName"
+    FROM "inventories" i
+    JOIN "users" u ON i.creator_id = u.id
+    WHERE i.search_vector @@ plainto_tsquery('english', ${text})
   `;
+
   return result;
 };
