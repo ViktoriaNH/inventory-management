dc3674deeff32f8b8c6828b53d15aa9bdaf2b9cd
viktoria naskova <smorodinka-gg@mail.ru>
2026-04-06 17:37:33 +0300

    fix(services): correct query in search logic

--- backend/src/services/search.js
+++ backend/src/services/search.js
@@ -1,10 +1,10 @@
 import { prisma } from "../../prisma/client.js";
 
-export const searchByText = async (searchText) => {
+export const searchByText = async (text) => {
   const result = await prisma.$queryRaw`
     SELECT title
     FROM "Inventory"
-    WHERE search_vector @@ plainto_tsquery(${searchText})
+    WHERE search_vector @@ plainto_tsquery(${text})
   `;
   return result;
 };
